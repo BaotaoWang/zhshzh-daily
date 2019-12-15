7b503e6d00d3f7887070807b00da04f7fd552435
BaotaoWang <btking@yeah.net>
2019-12-15 10:32:36 +0800

    server-feature:添加系统日志审计的拦截器

--- zhshzh-server/src/main/resources/zhshzh-sql.sql
+++ zhshzh-server/src/main/resources/zhshzh-sql.sql
@@ -22,3 +22,26 @@ CREATE TABLE `sys_user_info` (
 	UNIQUE INDEX `phone_number_UNIQUE` (`phone_number` ASC),
 	UNIQUE INDEX `email_UNIQUE` (`email` ASC)
 ) ENGINE = INNODB AUTO_INCREMENT = 100000 DEFAULT CHARACTER SET = utf8 COMMENT = '系统用户表';
+
+
+-- 创建系统接口日志表
+DROP TABLE IF EXISTS `sys_interface_log`;
+
+CREATE TABLE `sys_interface_log` (
+	`interface_log_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '接口日志id',
+	`path` VARCHAR (200) NOT NULL COMMENT '请求路径',
+	`type` VARCHAR (10) NOT NULL COMMENT '请求方式',
+	`request_data` TEXT DEFAULT NULL COMMENT '请求数据',
+	`process_time` INT NOT NULL COMMENT '请求数据处理时长（ms）',
+	`class_name` VARCHAR (200) NOT NULL COMMENT '请求类名',
+	`method_name` VARCHAR (32) NOT NULL COMMENT '请求方法名',
+	`user_name` VARCHAR (32) DEFAULT NULL COMMENT '请求用户',
+	`principal` VARCHAR (300) DEFAULT NULL COMMENT '用户权限信息',
+	`client_ip` VARCHAR (32) NOT NULL COMMENT '客户端ip',
+	`is_delete` TINYINT (1) NOT NULL DEFAULT 0 COMMENT '是否已删除（0：未删除； 1：已删除）',
+	`create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+	`update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+	`create_by` VARCHAR (32) NOT NULL DEFAULT 'system' COMMENT '创建人',
+	`update_by` VARCHAR (32) NOT NULL DEFAULT 'system' COMMENT '修改人',
+	PRIMARY KEY (`interface_log_id`)
+) ENGINE = INNODB DEFAULT CHARACTER SET = utf8 COMMENT = '系统接口日志表';
